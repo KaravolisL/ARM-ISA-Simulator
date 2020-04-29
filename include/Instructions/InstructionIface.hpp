05030e50fe75392b5c33ffc2e4e8f406a2211fdf
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-04-29 12:31:24 -0400

    InstructionIface

---
+++ include/Instructions/InstructionIface.hpp
@@ -0,0 +1,61 @@
+/////////////////////////////////
+/// @file InstructionIface.hpp
+///
+/// @brief Declaration of interface class
+///
+/// @author Luke Karavolis
+/////////////////////////////////
+#ifndef INSTRUCTION_IFACE_HPP
+#define INSTRUCTION_IFACE_HPP
+
+// SYSTEM INCLUDES
+// (None)
+
+// C PROJECT INCLUDES
+// (None)
+
+// C++ PROJECT INCLUDES
+// (None)
+
+// FORWARD DECLARATIONS
+template <typename T>
+class SLList;
+
+class Process;
+
+namespace std
+{
+    class string;
+}
+
+////////////////////////////////
+/// @class InstructionIface
+///
+/// @brief Fully virtual class used by 
+/// specific instruction classes
+////////////////////////////////
+class InstructionIface
+{
+public:
+
+    ////////////////////////////////
+    /// Deconstructor
+    ////////////////////////////////
+    virtual ~InstructionIface() {}
+
+    ////////////////////////////////
+    /// METHOD NAME: Execute
+    ///
+    /// @param[in] rArguments   List of arguments to be used by the instruction
+    /// @param[in,out] rProcess Process that this instruction is apart of
+    ////////////////////////////////
+    virtual void Execute(SLList<std::string> rArguments, Process& rProcess) = 0;
+
+protected:
+
+private:
+
+
+};
+
+#endif
