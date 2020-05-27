bf02687d99430c47dcb9033753e6f0aeb3771422
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-05-27 11:45:36 -0400

    Deleted old instruction classes

--- src/Instructions/InstructionRepository.cpp
+++ src/Instructions/InstructionRepository.cpp
@@ -16,47 +16,15 @@
 #include "Assert.hpp" // For ASSERT
 #include "InstructionRepository.hpp" // Header for class
 #include "InvalidSyntaxException.hpp" // For InvalidSyntaxException
-#include "MOVInstruction.hpp" // For MOV
-#include "ADDInstruction.hpp" // For ADD
-#include "SUBInstruction.hpp" // For SUB
-#include "ANDInstruction.hpp" // For AND
-#include "BICInstruction.hpp" // For BIC
-#include "EORInstruction.hpp" // For EOR
-#include "ORRInstruction.hpp" // For ORR
-#include "BInstruction.hpp" // For B
-#include "BLInstruction.hpp" // For BL
-#include "BXInstruction.hpp" // For BX
-#include "BLXInstruction.hpp" // For BLX
-#include "CMPInstruction.hpp" // For CMP
-#include "CMNInstruction.hpp" // For CMN
-#include "NOPInstruction.hpp" // For NOP
 #include "PUSHInstruction.hpp" // For PUSH
 #include "POPInstruction.hpp" // For POP
 #include "Registers.hpp" // For Registers
 #include "Process.hpp" // For Process
 #include "Logger.hpp" // For Logger
 
-static ADDInstruction f_ADD = ADDInstruction();
-static SUBInstruction f_SUB = SUBInstruction();
-
-static CMPInstruction f_CMP = CMPInstruction();
-static CMNInstruction f_CMN = CMNInstruction();
-
-static BInstruction f_B = BInstruction();
-static BLInstruction f_BL = BLInstruction();
-static BXInstruction f_BX = BXInstruction();
-static BLXInstruction f_BLX = BLXInstruction();
-
-static ANDInstruction f_AND = ANDInstruction();
-static BICInstruction f_BIC = BICInstruction();
-static EORInstruction f_EOR = EORInstruction();
-static ORRInstruction f_ORR = ORRInstruction();
-
 static PUSHInstruction f_PUSH = PUSHInstruction();
 static POPInstruction f_POP = POPInstruction();
 
-static MOVInstruction f_MOV = MOVInstruction();
-static NOPInstruction f_NOP = NOPInstruction();
 
 ////////////////////////////////
 /// METHOD NAME: InstructionRepository::Initialize 
@@ -64,27 +32,8 @@ static NOPInstruction f_NOP = NOPInstruction();
 void InstructionRepository::Initialize()
 {
     // Instruction dictionary
-    m_instructionDict.Insert("ADD", &f_ADD);
-    m_instructionDict.Insert("SUB", &f_SUB);
-
-    m_instructionDict.Insert("CMP", &f_CMP);
-    m_instructionDict.Insert("CMN", &f_CMN);
-
-    m_instructionDict.Insert("B", &f_B);
-    m_instructionDict.Insert("BL", &f_BL);
-    m_instructionDict.Insert("BX", &f_BX);
-    m_instructionDict.Insert("BLX", &f_BLX);
-
-    m_instructionDict.Insert("AND", &f_AND);
-    m_instructionDict.Insert("BIC", &f_BIC);
-    m_instructionDict.Insert("EOR", &f_EOR);
-    m_instructionDict.Insert("ORR", &f_ORR);
-
     m_instructionDict.Insert("PUSH", &f_PUSH);
     m_instructionDict.Insert("POP", &f_POP);
-
-    m_instructionDict.Insert("MOV", &f_MOV);
-    m_instructionDict.Insert("NOP", &f_NOP);
 
     // Conditional codes dictionary
     m_conditionalCodeDict.Insert("EQ", ConditionalCode::EQ);
@@ -117,7 +66,7 @@ InstructionBase* InstructionRepository::GetInstruction(std::string& rInstruction
     if (CheckConditionalCode(rInstruction, rProcess.GetProcessRegisters()) == false)
     {
         LOG_DEBUG("Do not execute this instruction");
-        return &f_NOP;
+        return nullptr;
     }
 
     // Determine if the instruction needs to set the flags
