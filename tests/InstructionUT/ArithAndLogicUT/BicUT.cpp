62ca5e284b6d97870bd3efe7b02d60002f870760
Luke Karavolis <lukejrkaravolis@gmail.com>
2020-08-01 20:53:11 -0400

    Converted BicUT

--- tests/InstructionUT/ArithAndLogicUT/BicUT.cpp
+++ tests/InstructionUT/ArithAndLogicUT/BicUT.cpp
@@ -13,115 +13,101 @@
 // (None)
 
 // C++ PROJECT INCLUDES
+#include <catch2/catch.hpp>
 #include "UnitTest.hpp"
 #include "InstructionBase.hpp"
 #include "InstructionBuilder.hpp"
 #include "Process.hpp"
 
-////////////////////////////////
-/// Test Objects
-////////////////////////////////
-static Process myProc = Process();
-static InstructionBuilder& builder = InstructionBuilder::GetInstance();
-static InstructionBase* pInstruction = nullptr;
-static std::string instructionStr;
-
-////////////////////////////////
-/// Setup Function
-////////////////////////////////
-static void setup()
+TEST_CASE("BIC Instruction", "[instruction][ArithAndLogic]")
 {
+    Process myProc = Process();
+    InstructionBuilder& builder = InstructionBuilder::GetInstance();
+    InstructionBase* pInstruction = nullptr;
+
     for (int i = 0; i < 13; i++)
     {
         myProc.GetProcessRegisters().genRegs[i] = i;
     }
-}
 
-////////////////////////////////
-/// BicRegsTest Function
-////////////////////////////////
-bool BicRegsTest()
-{
-    instructionStr = "BIC R0, R5, R1";
+    SECTION("BIC two registers together")
+    {
+        std::string instructionStr = "BIC R0, R1, R2";
+        int a = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
+        int b = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
 
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(myProc.GetProcessRegisters().genRegs[0] == 0b0100);
-    delete pInstruction;
+        myProc.GetProcessRegisters().genRegs[1] = a;
+        myProc.GetProcessRegisters().genRegs[2] = b;
 
-    instructionStr = "BIC R7, R9";
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(myProc.GetProcessRegisters().genRegs[0] == static_cast<uint32_t>(a & ~(b)));
+        delete pInstruction;
+    }
 
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(myProc.GetProcessRegisters().genRegs[7] == 0b0110);
-    delete pInstruction;
+    SECTION("BIC with a register")
+    {
+        std::string instructionStr = "BIC R0, R1";
+        int a = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
+        int b = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
 
-    return true;
-}
+        myProc.GetProcessRegisters().genRegs[0] = a;
+        myProc.GetProcessRegisters().genRegs[1] = b;
 
-////////////////////////////////
-/// BicLiterals Function
-////////////////////////////////
-bool BicLiterals()
-{
-    instructionStr = "BIC R0, R5, #0xB";
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(myProc.GetProcessRegisters().genRegs[0] == static_cast<uint32_t>(a & ~(b)));
+        delete pInstruction;
+    }
 
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(myProc.GetProcessRegisters().genRegs[0] == 0b0100);
-    delete pInstruction;
+    SECTION("BIC literal and a register")
+    {
+        int literal = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
+        int a = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
+        std::string baseInstructionStr = "BIC R0, R1, #";
+        std::string instructionStr = baseInstructionStr + std::to_string(literal);
+        myProc.GetProcessRegisters().genRegs[1] = a;
 
-    instructionStr = "BIC R10, #0xF";
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(myProc.GetProcessRegisters().genRegs[0] == static_cast<uint32_t>(a & ~(literal)));
+        delete pInstruction;
+    }
 
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(myProc.GetProcessRegisters().genRegs[10] == 0x0);
-    delete pInstruction;
+    SECTION("BIC register with a literal")
+    {
+        int literal = GENERATE(take(5, random(INT32_MIN, INT32_MAX)));
+        std::string baseInstructionStr = "BIC R1, #";
+        std::string instructionStr = baseInstructionStr + std::to_string(literal);
+        myProc.GetProcessRegisters().genRegs[1] = 1;
 
-    return true;
-}
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(myProc.GetProcessRegisters().genRegs[1] == static_cast<uint32_t>(1 & ~(literal)));
+        delete pInstruction;
+    }
 
-////////////////////////////////
-/// BicsTest Function
-////////////////////////////////
-bool BicsTest()
-{
-    // Reset registers
-    setup();
+    SECTION("BICS Test")
+    {
+        // "MOV" R1, #1
+        myProc.GetProcessRegisters().genRegs[1] = 0x1;
+        std::string instructionStr = "BICS R1, R1";
 
-    instructionStr = "BICS R1, R1";
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(myProc.GetProcessRegisters().GetZeroFlag());
+        REQUIRE(!myProc.GetProcessRegisters().GetNegativeFlag());
+        delete pInstruction;
 
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(myProc.GetProcessRegisters().GetZeroFlag());
-    UNIT_ASSERT(!myProc.GetProcessRegisters().GetNegativeFlag());
-    delete pInstruction;
+        // "MOV" R5, #0xFFFFFFFF
+        myProc.GetProcessRegisters().genRegs[5] = 0xFFFFFFFF;
 
-    // "MOV" R5, #0xFFFFFFFF
-    myProc.GetProcessRegisters().genRegs[5] = 0xFFFFFFFF;
+        instructionStr = "BICS R5, #0xF";
 
-    instructionStr = "BICS R5, #0xF";
-
-    pInstruction = builder.BuildInstruction(instructionStr, &myProc);
-    pInstruction->Execute(myProc.GetProcessRegisters());
-    UNIT_ASSERT(!myProc.GetProcessRegisters().GetZeroFlag());
-    UNIT_ASSERT(myProc.GetProcessRegisters().GetNegativeFlag());
-    delete pInstruction;
-
-    return true;
-}
-
-////////////////////////////////
-/// Main Function
-////////////////////////////////
-bool BicUT()
-{
-    UnitTest bicInstructionUT("BIC Instruction Unit Test");
-    bicInstructionUT.SetSetup(setup);
-
-    bicInstructionUT.AddSubTest(BicRegsTest);
-    bicInstructionUT.AddSubTest(BicLiterals);
-    bicInstructionUT.AddSubTest(BicsTest);
-
-    return bicInstructionUT.Run();
+        pInstruction = builder.BuildInstruction(instructionStr, &myProc);
+        pInstruction->Execute(myProc.GetProcessRegisters());
+        REQUIRE(!myProc.GetProcessRegisters().GetZeroFlag());
+        REQUIRE(myProc.GetProcessRegisters().GetNegativeFlag());
+        delete pInstruction;
+    }
 }
